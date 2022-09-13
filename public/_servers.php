dc25c9c6c5410103a0e463abd079f8476b800a3c
cylmat <cyrilmatte.pro@gmail.com>
2022-09-13 09:31:18 +0200

    check

--- public/_servers.php
+++ public/_servers.php
@@ -2,32 +2,46 @@
 
 # Use 'docker exec phpfpm "curl apache:80"' to test servers
 
-try {
-    $maria = new PDO(getenv('MARIADB_DSN'), 'user', 'pass');
-    $maria_version = $maria->getAttribute(\PDO::ATTR_SERVER_VERSION);
-} catch (Throwable $exception) {
-    $maria = false;
-    echo $exception->getMessage();
-}
+$server = ucfirst($_SERVER['SERVER_SOFTWARE']);
+$php = "PHP ".PHP_VERSION;
+
+$errors = 
+$maria_version = 
+$mysql_version = '';
 
 try {
-    $mysql = new PDO(getenv('MYSQL_DSN'), 'user', 'pass');
-    $mysql_version = $mysql->getAttribute(\PDO::ATTR_SERVER_VERSION);
+    $maria_version = 
+        (new PDO(getenv('MARIADB_DSN'), 'user', 'pass'))
+            ->getAttribute(PDO::ATTR_SERVER_VERSION);
+
+    $mysql_version = "Mysql ".
+        (new PDO(getenv('MYSQL_DSN'), 'user', 'pass'))
+            ->getAttribute(PDO::ATTR_SERVER_VERSION);
 } catch (Throwable $exception) {
-    $mysql = false;
-    echo $exception->getMessage();
+    $errors .= $exception->getMessage()."\n";
 }
 
+// Cli check
+$cli = 
+"$server
+$php
+$maria_version
+$mysql_version
+\n";
+
+// Html
 $template = '<style>.green{color:green} .red{color:red}</style>'.
+'<h1>It works</h1>'.
 '<ul>'.
-    "<li>{$_SERVER['SERVER_SOFTWARE']}</li>".
-    "<li>PHP ".PHP_VERSION."</li>".
-    ($maria
+    "<li>$server</li>".
+    "<li>$php</li>".
+    ($maria_version
         ? "<li><span class=\"green\">MariaDb v$maria_version</span></li>"
         : '<li><span class="red">No MariaDb</span></li>').
-    ($mysql
+    ($mysql_version
         ? "<li><span class=\"green\">Mysql v$mysql_version</span></li>"
         : '<li><span class="red">No Mysql</span></li>').
 '</ul>';
 
-return $template;
+if ('' !== $errors) return $errors.PHP_EOL;
+return key_exists('check', $_GET) ? $cli : $template;
--- public/index.php
+++ public/index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo '<h1>It works</h1>' . include __DIR__.'/_servers.php';
+echo include __DIR__.'/_servers.php';
